ff590382dbdd7b0ab980457b9645f2b654cfa6f3
Yuta Yamamoto <wookiee33@gmail.com>
2021-01-07 21:39:17 +0900

    自動生成のコマンドをシェルスクリプトにする。

--- mybatis-generator/generate.sh
+++ mybatis-generator/generate.sh
@@ -1,1 +1,10 @@
-java -jar lib/david-mybatis-generator-core-1.0.0.jar -configfile generatorConfig.xml -overwrite
+cd `dirname $0`
+
+# 自動生成の成果物を削除する。
+rm -f -r ../src/gen/java
+mkdir ../src/gen/java
+rm -f -r ../src/gen/resources
+mkdir ../src/gen/resources
+
+# 自動生成する。
+java -jar lib/david-mybatis-generator-core-1.0.0.jar -configfile generatorConfig.xml
