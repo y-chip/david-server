92f231e9dd0c4f876b6b992e7e274dafcc996331
Yuta Yamamoto <wookiee33@gmail.com>
2021-01-07 19:26:23 +0900

    MyBatis、および、MyBatis Generatorを追加する。

---
+++ mybatis-generator/generate.sh
@@ -0,0 +1,1 @@
+java -jar lib/david-mybatis-generator-core-1.0.0.jar -configfile generatorConfig.xml -overwrite
--- src/main/java/com/yamamoto/yuta/david/DavidApplication.java
+++ src/main/java/com/yamamoto/yuta/david/DavidApplication.java
@@ -1,8 +1,10 @@
 package com.yamamoto.yuta.david;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.yamamoto.yuta.david.infrastructure.repository.mapper")
 @SpringBootApplication
 public class DavidApplication {
 
