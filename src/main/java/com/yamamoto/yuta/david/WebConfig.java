4d9a6400adfd0b694f17ed696f021abd3945beac
Yuta Yamamoto <wookiee33@gmail.com>
2021-01-28 21:48:46 +0900

    デモに向けて、CORSを見直す。

--- src/main/java/com/yamamoto/yuta/david/WebConfig.java
+++ src/main/java/com/yamamoto/yuta/david/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
 
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:4200", "https://y-chip.github.io")
                 .allowCredentials(true).maxAge(3600);
     }
 }
